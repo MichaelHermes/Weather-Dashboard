379c96df45b6ea80f4be450138194e3e93d42c61
Michael Hermes <mikehermes87@gmail.com>
2021-07-22 12:17:24 -0700

    Hook up the 'city search' form and query the OneWeather API for current weather.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,9 @@
+const currentWeatherRequestUrl =
+	"https://api.openweathermap.org/data/2.5/weather";
+const oneCallAPIRequestUrl = "https://api.openweathermap.org/data/2.5/onecall";
+const APIKey = "89b52cb1c296e2d8fed4b1b377ceb088";
+const weatherIconUrl = "http://openweathermap.org/img/wn/{icon}@2x.png";
+
 let searchHistoryList = $("#search-history");
 
 function generateSearchHistoryBtns() {
@@ -15,4 +21,43 @@ function generateSearchHistoryBtns() {
 	);
 }
 
+function searchCityWeather(event) {
+	event.preventDefault();
+
+	let cityInputVal = $("#city-input").val();
+
+	// Make sure they entered a city value.
+	if (!cityInputVal) {
+		alert("A city input is required!");
+		return;
+	}
+
+	queryCurrentWeather(cityInputVal);
+}
+
+function queryCurrentWeather(cityInputVal) {
+	let requestQueryURL = `${currentWeatherRequestUrl}?q=${cityInputVal}&appid=${APIKey}&units=imperial`;
+
+	fetch(requestQueryURL)
+		.then(response => {
+			if (!response.ok) {
+				throw new Error("Network response was not ok");
+			}
+			return response.json();
+		})
+		.then(data => {
+			console.log(data);
+
+			// TODO: Use the Latitude/Longitude data values to query the daily weather forecast.
+		})
+		.catch(error => {
+			console.error(
+				"There has been a problem with the fetch operation: ",
+				error
+			);
+		});
+}
+
 generateSearchHistoryBtns();
+
+$("#city-search-form").on("submit", searchCityWeather);
